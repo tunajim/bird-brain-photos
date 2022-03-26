17d0cf22d4adeb79cfbc8c52aa79e85e776d3c45
tunajim <87504847+tunajim@users.noreply.github.com>
2022-03-25 23:01:23 -0700

    Updates

---
+++ precache-manifest.2ce0e55bc1c9372e095d306176591cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eb91f1fa6d2db573fe8",
+    "url": "/bird-brain-photos/static/css/main.3b2ab482.chunk.css"
+  },
+  {
+    "revision": "2eb91f1fa6d2db573fe8",
+    "url": "/bird-brain-photos/static/js/main.2eb91f1f.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/bird-brain-photos/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/bird-brain-photos/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "411b2d9844222bd7a0b0",
+    "url": "/bird-brain-photos/static/js/runtime~main.411b2d98.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/bird-brain-photos/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9d315e9cd99e69da5bb4995417c6d667",
+    "url": "/bird-brain-photos/index.html"
+  }
+];
